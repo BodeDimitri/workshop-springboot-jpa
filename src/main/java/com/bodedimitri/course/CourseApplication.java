75880e5d090b18386ba0768f712d56c343636dad
BodeDimitri <diegocesaralbino@gmail.com>
2024-02-07 17:30:56 -0300

    07/02

---
+++ src/main/java/com/bodedimitri/course/CourseApplication.java
@@ -0,0 +1,14 @@
+package com.bodedimitri.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+		//
+	}
+
+}
